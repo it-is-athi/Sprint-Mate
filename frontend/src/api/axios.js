476df506bb8ebf51b348cda878b68f1630ecd584
it-is-athi <athiv1118@gmail.com>
2025-11-02 21:58:47 +0530

    getting ready to host.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.NODE_ENV === 'production' 
+    ? 'https://v8bkgs6c-5000.inc1.devtunnels.ms/api'  // forwarded backend URL (production)
+    : 'http://localhost:5000/api',
   withCredentials: true
 });
 
