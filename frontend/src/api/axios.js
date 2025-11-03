5b5a8cf636d7f5759ac234798edc8d5ac1cfc735
it-is-athi <athiv1118@gmail.com>
2025-11-03 09:12:47 +0530

    backend port updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://v8bkgs6c-5000.inc1.devtunnels.ms/api'  // forwarded backend URL (production)
-    : 'http://localhost:5000/api',
+  baseURL: 'https://v8bkgs6c-5000.inc1.devtunnels.ms/' ,
   withCredentials: true
 });
 
