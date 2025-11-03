c418bab77416dd5478c8e98d2a8a6b24ce3e6349
it-is-athi <athiv1118@gmail.com>
2025-11-03 12:00:34 +0530

    Local Host access given

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://v8bkgs6c-5000.inc1.devtunnels.ms/' ,
+  baseURL: process.env.NODE_ENV === 'production' 
+    ? 'https://v8bkgs6c-5000.inc1.devtunnels.ms/api'  // Production (Vercel)
+    : 'http://localhost:5000/api',  // Development (localhost)
   withCredentials: true
 });
 
